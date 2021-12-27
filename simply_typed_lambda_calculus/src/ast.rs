8759b7f5ac0f482f483b502efdfd1f5e13d6d988
PoorlyDefinedBehaviour <brunotj2015@hotmail.com>
2021-12-26 22:24:11 -0300

    feat(stlc): add Int term and type

--- simply_typed_lambda_calculus/src/ast.rs
+++ simply_typed_lambda_calculus/src/ast.rs
@@ -2,24 +2,27 @@
 //  | *                   -- base type
 //  | Type -> Type        -- function type
 //  | (Type)              -- grouping
+#[derive(Debug, Clone, PartialEq)]
 pub enum Type {
+  // *
+  Int,
   // Type -> Type
   Arrow(Box<Type>, Box<Type>),
 }
 
 // Term ::=
+//  | Int                 -- integer literal
 //  | Var                 -- term variable
 //  | Term Term           -- term application
 //  | λ Var : Type . Term -- term abstraction
 //  | (Term)              -- grouping
+#[derive(Debug)]
 pub enum Term {
+  Int(i32),
   // Var
   Var(String),
   // Term Term
-  App {
-    Abs: Box<Term>,
-    Arg: Box<Term>,
-  },
+  App(Box<Term>, Box<Term>),
   // λ Var: Type. Term
   Abs {
     param_name: String,
